6672539732fdeb4b3a5217c415a2b186edc9129c
JoelSalum <joelsalum.dev@gmail.com>
2024-04-22 20:00:39 -0300

    terminamos los ejercicios de la parte 03, Eslint y Validaciones

--- index.js
+++ index.js
@@ -1,100 +1,95 @@
-import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
-import morgan from 'morgan';
-import cors from 'cors';
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 import 'dotenv/config'
 import Person from './models/personModel.js'
-const app = express();
-app.use(express.json(), cors(), express.static('dist'));
-morgan.token('body',(req) => { return JSON.stringify(req.body)})
+const app = express()
+app.use(express.json(), cors(), express.static('dist'))
+morgan.token('body', (req) => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
 
 const getDate = () => {
   const options = {
     weekday: 'short',
-    day: '2-digit', 
-    month: 'short', 
-    year: 'numeric', 
-    hour: '2-digit', 
-    minute: '2-digit', 
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
     second: '2-digit',
-    timeZoneName: 'short' 
-};
-  const date = new Date();
-  return date.toLocaleString('en-US', options);
+    timeZoneName: 'short'
+  }
+  const date = new Date()
+  return date.toLocaleString('en-US', options)
 }
 
-//GET ALL PERSONS
-app.get("/api/persons", (req, res, next) => {
+//  GET ALL PERSONS
+app.get('/api/persons', (req, res, next) => {
   Person.find({}).then(persons => {
     res.json(persons)
   }).catch(err => next(err))
 })
 
-//FIND A PERSON BY ID
+//  FIND A PERSON BY ID
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id).then(person => {
-      if(person){
-        res.status(200).json(person)
-      }else{
-        res.status(404).json({Error: 'Not Found'})
-      }
-    }).catch(err => next(err))
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      res.status(200).json(person)
+    } else {
+      res.status(404).json({ Error: 'Not Found' })
+    }
+  }).catch(err => next(err))
 })
 
-
-//ADD NEW PERSON TO PHONEBOOK
+//  ADD NEW PERSON TO PHONEBOOK
 app.post('/api/persons', (req, res, next) => {
-  if(!req.body.name || !req.body.number){
+  if (!req.body.name || !req.body.number) {
     return res.status(400).json({
-      Error: "Name and Number must not be void"
-    }) 
+      Error: 'Name and Number must not be void'
+    })
   }
-  
-  Person.find({name: req.body.name})
+
+  Person.find({ name: req.body.name })
     .then(result => {
-      if(result.length > 0){
+      if (result.length > 0) {
         const newPerson = {
-            name: req.body.name,
-            number: req.body.number
+          name: req.body.name,
+          number: req.body.number
         }
         const id = result[0].id
         Person.findByIdAndUpdate(
-          id, 
-          newPerson, 
+          id,
+          newPerson,
           { new: true, runValidators: true, context: 'query' })
           .then(updatedPerson => {
             res.json(updatedPerson)
           }).catch(err => next(err))
-      }else{
+      } else {
         const person = new Person({
           name: req.body.name,
           number: req.body.number
         })
-          person.save().then(savedPerson => {
-              res.status(201).json(savedPerson)
-          }).catch(err => next(err))
-      }
-  }).catch(err => next(err))
-
-})
-
-//DELETE A PERSON BY ID
-app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndDelete(req.params.id)
-    .then(personDeleted => {
-      if(personDeleted){
-        res.status(204).end()
-      }else{
-        res.status(404).json({Error: "not found"})
+        person.save().then(savedPerson => {
+          res.status(201).json(savedPerson)
+        }).catch(err => next(err))
       }
     }).catch(err => next(err))
 })
 
+//  DELETE A PERSON BY ID
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then(personDeleted => {
+      if (personDeleted) {
+        res.status(204).end()
+      } else {
+        res.status(404).json({ Error: 'not found' })
+      }
+    }).catch(err => next(err))
+})
 
-//GET INFO
-app.get("/info", (req, res) => {
+//  GET INFO
+app.get('/info', (req, res, next) => {
   Person.countDocuments()
     .then(personDocuments => {
       res.send(
@@ -107,8 +102,7 @@ app.get("/info", (req, res) => {
     }).catch(err => next(err))
 })
 
-
-//HANDLE RERROR MANAGER
+//  HANDLE RERROR MANAGER
 const errorHandler = (err, req, res, next) => {
   console.error(err.message)
   if (err.name === 'CastError') {
@@ -120,7 +114,6 @@ const errorHandler = (err, req, res, next) => {
 }
 
 app.use(errorHandler)
-
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
